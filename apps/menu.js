295feadd2e494f528a7c09942093873a1ac3d464
suzuka <3103088569@qq.com>
2025-09-08 21:43:53 +0800

    修改菜单

--- apps/menu.js
+++ apps/menu.js
@@ -14,7 +14,7 @@ export class helpMenu extends plugin {
       priority: 1135,
       rule: [
         {
-          reg: "^#?菜单$",
+          reg: "^#(菜单|帮助)$",
           fnc: "showMenu",
         },
       ],
