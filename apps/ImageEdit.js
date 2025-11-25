82412f365aee5f18f5655fce3a1353f27647836f
suzuka <3103088569@qq.com>
2025-11-25 20:17:40 +0800

    测试

--- apps/ImageEdit.js
+++ apps/ImageEdit.js
@@ -228,6 +228,13 @@ export class EditImage extends plugin {
         imageConfig: {
           imageSize: imageSize,
         },
+        safetySettings: [
+          { category: "HARM_CATEGORY_HATE_SPEECH", threshold: "BLOCK_NONE" },
+          { category: "HARM_CATEGORY_SEXUALLY_EXPLICIT", threshold: "BLOCK_NONE" },
+          { category: "HARM_CATEGORY_HARASSMENT", threshold: "BLOCK_NONE" },
+          { category: "HARM_CATEGORY_DANGEROUS_CONTENT", threshold: "BLOCK_NONE" },
+          { category: "HARM_CATEGORY_CIVIC_INTEGRITY", threshold: "BLOCK_NONE" },
+        ],
       }
 
       if (aspectRatio) {
