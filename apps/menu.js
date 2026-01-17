f185dcb873c426c75c30e3b3bfb6514b8c4136b3
suzuka <118676764+suzuka-suzuka@users.noreply.github.com>
2026-01-17 23:10:23 +0800

    Refactor getImageUrl to remove emoji setting logic
    Removed conditional emoji setting for group messages.

--- apps/menu.js
+++ apps/menu.js
@@ -23,9 +23,6 @@ export class helpMenu extends plugin {
   }
 
   async getImageUrl() {
-    if (e.isGroup && typeof e.group?.setMsgEmojiLike === "function") {
-      await e.group.setMsgEmojiLike(e.message_id, "124")
-    }
 
     const url = "https://yande.re/post.json?tags=loli+-rating:e+-nipples&limit=500"
     try {
