520fff7aa2b96cb299f8e865f6d44eea3ea662a8
suzuka <3103088569@qq.com>
2026-02-23 20:42:33 +0800

    优化飞行棋bug

--- apps/flychess.js
+++ apps/flychess.js
@@ -41,8 +41,7 @@ export class Ludo extends plugin {
       return false
     }
 
-    const response = await gameManager.joinGame(e.user_id)
-    await e.reply(response)
+    await gameManager.joinGame(e.user_id)
     return true
   });
 
